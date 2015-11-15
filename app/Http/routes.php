2638d9e0aa329e152bd05df55c257bd9969d5b28
JorisDek <dragster66@gmail.com>
2015-11-15 03:28:55 +0100

    phpinfo2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-//Route::get('/', 'PagesController@welcome');
-Route::get('foo', ['middleware' => 'manager', function(){
-	
-	return phpinfo();
-}]);
+Route::get('/', 'PagesController@welcome');
 
 Route::resource('articles', 'ArticlesController');
 
